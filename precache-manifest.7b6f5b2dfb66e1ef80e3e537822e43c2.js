978ddfb6a8b926b187ee82521f61c6198426875c
Okrymus inc <OKRYMUS@Panupongs-MacBook-Pro.local>
2019-08-28 16:46:01 -0400

    Updates

---
+++ precache-manifest.7b6f5b2dfb66e1ef80e3e537822e43c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac9042799a3b3aa2082189ed56a29f9",
+    "url": "/ampweb/index.html"
+  },
+  {
+    "revision": "ffaa5a3eaeed270205c1",
+    "url": "/ampweb/static/css/2.eee0f4a3.chunk.css"
+  },
+  {
+    "revision": "621898182e86c5f09409",
+    "url": "/ampweb/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ffaa5a3eaeed270205c1",
+    "url": "/ampweb/static/js/2.ab5bd926.chunk.js"
+  },
+  {
+    "revision": "621898182e86c5f09409",
+    "url": "/ampweb/static/js/main.b44cb0dd.chunk.js"
+  },
+  {
+    "revision": "c9434d1b00c4b14df444",
+    "url": "/ampweb/static/js/runtime~main.992ff6fc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ampweb/static/media/logo.5d5d9eef.svg"
+  }
+]);
